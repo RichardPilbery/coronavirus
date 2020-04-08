9450335deba7474343e1c0dd4f92afc4a7574f51
Richard Pilbery <tricky999@Richards-iMac.local>
2020-04-08 18:13:58 +0100

    Updated cases for 8th April Revised plotting functions to output plotly graph

--- plotting.R
+++ plotting.R
@@ -1,10 +1,30 @@
 library(tidyverse)
 library(lubridate)
 
-df <- read_csv('covid-general-data.csv')
+df <- read_csv('coronavirus-data.csv') %>%
+  select(report_date, positive, deaths) %>%
+  mutate(thedate = as.Date(report_date, "%d %B %Y")) %>%
+  select(-report_date)
 
-ggplot(df, aes(x=thedate)) +
-  geom_point(aes(y=`Uk Daily Deaths`), stat="identity", color='red') + 
-  geom_line(aes(y=`Uk Daily Deaths`), stat="identity", color='red') 
+df1 <- read_csv('covid-general-data.csv') %>%
+  select(thedate, `Total UK cases`, `Total UK deaths`) %>%
+  rename(
+    positive = `Total UK cases`,
+    deaths = `Total UK deaths`
+  ) %>% filter(thedate > '2020-04-05')
+
+df_final <- bind_rows(df, df1)
 
 
+a <- ggplot(df_final, aes(x=thedate)) +
+  geom_point(aes(y=positive), stat="identity", color='red') + 
+  geom_line(aes(y=positive), stat="identity", color='red') + 
+  geom_line(aes(y=deaths), stat="identity", color='black') +
+  geom_point(aes(y=deaths), stat="identity", color='black') +
+  scale_x_date(date_breaks = "1 week") + 
+  theme(axis.text.x = element_text(angle = 45))
+
+
+b <- plotly::ggplotly(a)
+
+htmlwidgets::saveWidget(as_widget(b), "cases.html")
